4949bcf08b77d43c438e09e883d54a9362f49604
Sylvain <sylvain.joyeux@tidewise.io>
2020-05-14 10:43:14 -0300

    Initial commit

---
+++ src/Main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <deps_target_dependency/Dummy.hpp>
+
+int main(int argc, char** argv)
+{
+    deps_target_dependency::DummyClass dummyClass;
+    dummyClass.welcome();
+
+    return 0;
+}
